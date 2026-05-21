815eec3da4e75fa799b4dd2a5088c4ab087bab1c
Muhammad Jawad <112897469+muhammadJawadm@users.noreply.github.com>
2026-05-21 22:40:41 +0500

    add the pm2 setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
       ],
       env: {
         NODE_ENV: 'development',
-        PORT: 3000
+        PORT: 5000
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 5000
       }
     }
   ]
